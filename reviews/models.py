a0c107e1b06e76f6a542802c735cc377b1ced7be
rogeriodelphi <rogeriodelphi@gmail.com>
2022-01-26 23:57:56 -0300

    Create models reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.db import models
 
 class Publisher(models.Model):
@@ -8,7 +9,6 @@ class Publisher(models.Model):
 
     def __str__(self):
         return self.name
-
 
 
 class Book(models.Model):
@@ -44,3 +44,13 @@ class BookContributor(models.Model):
                             choices=ContributionRole.choices, max_length=20)
 
 
+class Review(models.Model):
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(null=True,
+                                       help_text="The date and time the review was last edited.")
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             help_text="The Book that this review is for.")
