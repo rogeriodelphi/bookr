7ec3967fb9140584b0ba884d614ef19beb65cb08
rogeriodelphi <rogeriodelphi@gmail.com>
2022-01-28 01:22:07 -0300

    URL Mapping, Views, and Templates

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index)
+    path('', include('reviews.urls'))
 ]
